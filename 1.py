bfb359cadec553d3ca8131b4f63f4445e2626e3b
nikitadhona <dhonanikita@gmail.com>
2022-02-13 10:24:29 +0530

    Simple window

---
+++ 1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root=Tk()           #root Widget (Window)
+
+#Creating a Label Widget
+myLabel=Label(root,text="Hello World!")
+#Puttting to Screen
+myLabel.pack()
+
+root.mainloop()
